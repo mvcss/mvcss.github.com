3878818849cd6881bd51d469cc916f92762f86b3
Drew Barontini <drewbarontini@gmail.com>
2014-12-10 13:59:18 -0500

    Update to Leo v1.9.3

--- config.rb
+++ config.rb
@@ -10,27 +10,31 @@ set :relative_links, true
 
 # Methods defined in the helpers block are available in templates
 helpers do
-  def get_page_priority(title)
-    data.priority.index(title) || data.priority.length
+  def get_page_priority(page)
+    unless page.nested?
+      data.priority.index(page.title) || data.priority.length
+    else
+      data.priority.index("#{ page.parent } - #{ page.title }") || data.priority.length
+    end
   end
 
   def get_pages
     sitemap.resources.select { |resource| resource.data.type == 'page' }
-      .sort_by { |r| get_page_priority(r.data.title) }
+      .sort_by { |r| get_page_priority(r.data) }
   end
 
   def get_secondary_pages
     sitemap.resources.select { |resource| resource.data.type == 'page' and resource.data.secondary? }
-      .sort_by { |r| get_page_priority(r.data.title) }
+      .sort_by { |r| get_page_priority(r.data) }
   end
 
   def get_page(priority)
-    sitemap.resources.select { |resource| resource.data.type == 'page' and get_page_priority(resource.data.title) == priority }
+    sitemap.resources.select { |resource| resource.data.type == 'page' and get_page_priority(resource.data) == priority }
   end
 
   def get_nested_pages(title)
     sitemap.resources.select { |resource| resource.data.type == 'page' and resource.data.nested == true and resource.data.parent == title }
-      .sort_by { |r| get_page_priority(r.data.title) }
+      .sort_by { |r| get_page_priority(r.data) }
   end
 
   def is_excluded_page(page)
